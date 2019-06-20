4a9a5528fcf82b782860ef85565574146616d482
flycdp <51779001+flycdp@users.noreply.github.com>
2019-06-20 12:22:46 +0800

    module更新
    yhb

--- module/dbpool.js
+++ module/dbpool.js
@@ -1,5 +1,10 @@
 const mysql=require('mysql');
-
+const yhb=require('./yhb.js');
+const hlx=require('./hlx.js');
+const fy=require('./fy.js');
+const jf=require('./jf.js');
+const ljy=require('./ljy.js');
+const wq=require('./wq.js')
 const dbPool={
     pool:null,
     config:{
@@ -9,17 +14,24 @@ const dbPool={
         password:'123456',
         database:'pro3'
     },
+    
     create(){
         this.pool=mysql.createPool(this.config);
         console.log('连接池已启动');
     },
+    yhb,
+    hlx,
+    fy,
+    jf,
+    ljy,
+    wq,
     login(){
         return 'select * from t_user where u_name=? and u_pwd=?'
     },
     reg(){
         return ['select * from t_user where u_name=?','insert into t_user values(null,?,?)']
     },
-
+    
     connect(sql,arr,fn){
         this.pool.getConnection((err,connection)=>{
             connection.query(sql,arr,fn);
